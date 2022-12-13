f94196bd46e085335d9cefc0fa53c7a403b034a5
Khairul Haaziq <rhycoz@yahoo.com>
2022-12-13 23:09:46 +0800

    refactor: move components to files

---
+++ components/accordion/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <details :class="rootClass">
+    <slot></slot>
+    <div :class="class">Something small enough to escape casual notice.</div>
+</details>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    class: String,
+    rootClass: String,
+})
+</script>
